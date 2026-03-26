7188644a181e1f1108a9c371272dcf7afd516d38
Admin <admin@company.com>
2026-03-26 14:57:43 +0530

    Final_Auth_Fix

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,10 +10,9 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 // Compare plain password to hash
